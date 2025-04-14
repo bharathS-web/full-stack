b7227376ccd467f81b973be2c31b33de2c0600be
bharaths-web <sbharathkumar1425@gmail.com>
2025-04-14 12:01:59 +0530

    Outputs Updated

---
+++ exx10/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+import Counter from './Counter.jsx'
+import BookList from './BookList.jsx'
+
+function App() {
+  return (
+    <div>
+      {/* <Counter /> */}
+    <BookList />
+    </div>
+  );
+}
+
+export default App;
--- exx10/src/main.jsx
+++ exx10/src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles.css'
-import Counter from './Counter.jsx'
-import BookList from './BookList.jsx'
+
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter />
-    {/* <BookList /> */}
+    
+    <App/>
+
   </StrictMode>,
 )
